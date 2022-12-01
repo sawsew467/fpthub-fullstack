c79fe6d301c9dd7233b10275fc8f39ccb6473405
sawsew467 <bao.thang.1912@gmail.com>
2022-12-02 00:16:12 +0700

    convert to typescrirp

--- front-end/src/redux/selectors.js
+++
@@ -1,7 +0,0 @@
-export const darkmodeSelector = (state) => state.darkmode;
-export const postsSelector = (state) => state.postList;
-export const usersSelector = (state) => state.userList;
-export const moblieMenuSelector = (state) => state.moblieMenu;
-export const currentUserSelector = (state) => state.currentUser;
-
-// export default darkmodeSelector;

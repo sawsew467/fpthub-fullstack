0dd6ed20e56b3dc7b8da6259f1f13b5fe745e5a4
sawsew467 <bao.thang.1912@gmail.com>
2022-12-04 16:18:28 +0700

    login api request

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { bindActionCreators } from "redux";
 import { postActionCreators, userActionCreators } from "../redux";
 import DEMO_POSTS from "../data/listDemo";
 import DEMO_USERS from "../data/usersDemo";
+import axios from "axios";
 
 function Login() {
-
+  const [inputData, setInputData] = useState({
+    email: "",
+    password: "",
+  });
   const dispath = useDispatch();
   const { setPosts } = bindActionCreators(postActionCreators, dispath);
   const { setUsers } = bindActionCreators(userActionCreators, dispath);
+  const handleSubmit = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    try {
+      e.preventDefault();
+      const option = {
+        method: "post",
+        url: "/auth/login",
+        data: inputData,
+      };
+      const response = await axios(option);
+      const { token, email } = response.data.data;
+      localStorage.setItem("token", token);
+      console.log(token);
+      console.log(email);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   const sendDataDemo = (): void => {
     setPosts(DEMO_POSTS);
     setUsers(DEMO_USERS);
@@ -35,10 +58,10 @@ function Login() {
             <input
               className="w-full text-lg py-2 pr-3 pl-10 rounded-xl placeholder:text-gray-800 bg-[#ffffff9c] outline-none"
               placeholder="Tên tài khoản"
-              //   onChange={(e) =>
-              //     setUserInput({ ...userInput, email: e.target.value })
-              //   }
-              //   value={userInput.email}
+              onChange={(e) =>
+                setInputData({ ...inputData, email: e.target.value })
+              }
+              value={inputData.email}
             ></input>
             <div className="absolute top-0 left-0 w-10 h-full flex justify-center items-center">
               <i className="fa-solid fa-user text-lg text-orange"></i>
@@ -49,10 +72,10 @@ function Login() {
               className="w-full text-lg py-2 pr-3 pl-10 rounded-xl placeholder:text-gray-800 bg-[#ffffff9c] outline-none"
               placeholder="Mật khẩu"
               type="password"
-              //   onChange={(e) =>
-              //     setUserInput({ ...userInput, password: e.target.value })
-              //   }
-              //   value={userInput.password}
+              onChange={(e) =>
+                setInputData({ ...inputData, password: e.target.value })
+              }
+              value={inputData.password}
             ></input>
             <div className="absolute top-0 left-0 w-10 h-full flex justify-center items-center">
               <i className="fa-solid fa-lock text-lg text-orange"></i>
@@ -69,7 +92,7 @@ function Login() {
           </div>
           <button
             className="w-7/12 text-lg py-2 px-3 mt-3 bg-[#ffffff9c] text-center rounded-full"
-            // onClick={(e) => handleSubmit(e)}
+            onClick={(e) => handleSubmit(e)}
           >
             Đăng nhập
           </button>
